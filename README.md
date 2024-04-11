d5340b8f970b3d8f993b6aca10b7fc1a72bea5a1
Niels-Ole Lambertz <git@niels-ole.com>
2024-04-11 10:57:40 +0200

    Adding files

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# Experimental renderer
+
+This repo contains an experimental rudimentary OSM renderer. Its goal is to be able to render some sort of map with little operational overhead.
+
+Experimental renderer (this will not be updated on every release, so ymmv) compared with osm.org:
+
+![black lines only](assets/5295.png)
+![osm.org](assets/5295-compare.png)
+
+Images [OpenStreetMap](https://www.openstreetmap.org/) contributors, [CC-BY-SA](https://creativecommons.org/licenses/by-sa/2.0/)
+
+To use this, pass an oms.pbf file via `-osm_path`.
+
+Currently osm.pbf files have to be prepared like so
+```
+osmium add-locations-to-ways ~/Downloads/hamburg-latest.osm.pbf -o prepared.osm.pbf -f
+```
+
+This creates a temporary file on startup, which might take some time.
+Testing so far was only done for individual cities.
+
+## Usage
+
+```
+Usage of go-gl-osm:
+  -host string
+        HTTP Listening host (default "0.0.0.0")
+  -osm_path string
+        Path to osm_path to use for direct rendering. (experimental)
+  -port int
+        HTTP Listening port (default 8080)
+  -static string
+        Path to static file directory (default "./static/")
+  -tls_cert_path string
+        Path to TLS certificate
+  -tls_key_path string
+        Path to TLS key
+  -tls_port int
+        HTTPS Listening port. This listener is only enabled if both tls cert and key are set. (default 8443)
+  -verbose
+        Output debug log messages
+```
---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nielsole/go-gl-osm/renderer"
+)
+
+func main() {
+	http_listen_host := flag.String("host", "0.0.0.0", "HTTP Listening host")
+	http_listen_port := flag.Int("port", 8080, "HTTP Listening port")
+	https_listen_port := flag.Int("tls_port", 8443, "HTTPS Listening port. This listener is only enabled if both tls cert and key are set.")
+	static_dir := flag.String("static", "./static/", "Path to static file directory")
+	osm_path := flag.String("osm_path", "", "Path to osm_path to use for direct rendering. (experimental)")
+	tls_cert_path := flag.String("tls_cert_path", "", "Path to TLS certificate")
+	tls_key_path := flag.String("tls_key_path", "", "Path to TLS key")
+	verbose := flag.Bool("verbose", false, "Output debug log messages")
+
+	flag.Parse()
+
+	var requestHandler func(http.ResponseWriter, *http.Request)
+
+	if len(*osm_path) == 0 {
+
+	}
+	// Create a temp file.
+	var err error
+	tempFile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		fmt.Println("Cannot create temp file:", err)
+		os.Exit(1)
+	}
+
+	data, err := renderer.LoadData(*osm_path, 15, tempFile)
+	if err != nil {
+		logFatalf("There was an error loading data: %v", err)
+	}
+	tempFileName := tempFile.Name()
+	tempFile.Close()
+
+	// Memory-map the file
+	mmapData, mmapFile, err := renderer.Mmap(tempFileName)
+	if err != nil {
+		logFatalf("There was an error memory-mapping temp file: %v", err)
+	}
+	defer renderer.Munmap(mmapData)
+	defer mmapFile.Close()
+	requestHandler = func(w http.ResponseWriter, r *http.Request) {
+		if *verbose {
+			logDebugf("%s request received: %s", r.Method, r.RequestURI)
+		}
+		if r.Method != "GET" {
+			http.Error(w, "Only GET requests allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		renderer.HandleRenderRequest(w, r, data, 15, mmapData)
+		//handleRequest(w, r, *data_dir, *map_name, *renderd_socket, *renderd_timeout_duration, *tile_expiration_duration)
+	}
+	defer func() {
+		// Cleanup the temp file.
+		if err := os.Remove(tempFile.Name()); err != nil {
+			fmt.Println("Failed to remove temp file:", err)
+		} else {
+			fmt.Println("Temp file removed.")
+		}
+
+	}()
+
+	// HTTP request multiplexer
+	httpServeMux := http.NewServeMux()
+
+	// Tile HTTP request handler
+	httpServeMux.HandleFunc("/tile/", requestHandler)
+
+	// Static HTTP request handler
+	httpServeMux.Handle("/", http.FileServer(http.Dir(*static_dir)))
+
+	// HTTP Server
+	httpServer := http.Server{
+		Handler: httpServeMux,
+	}
+
+	go func() {
+
+		// HTTPS listener
+		if len(*tls_cert_path) > 0 && len(*tls_key_path) > 0 {
+			go func() {
+				httpsAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", *http_listen_host, *https_listen_port))
+				if err != nil {
+					logFatalf("Failed to resolve TCP address: %v", err)
+				}
+				httpsListener, err := net.ListenTCP("tcp", httpsAddr)
+				if err != nil {
+					logFatalf("Failed to start TCP listener: %v", err)
+				} else {
+					logInfof("Started HTTPS listener on %s\n", httpsAddr)
+				}
+				err = httpServer.ServeTLS(httpsListener, *tls_cert_path, *tls_key_path)
+				if err != nil && err != http.ErrServerClosed {
+					logFatalf("Failed to start HTTPS server: %v", err)
+				}
+			}()
+		} else {
+			logInfof("TLS is disabled")
+		}
+
+		// HTTP listener
+		httpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", *http_listen_host, *http_listen_port))
+		if err != nil {
+			logFatalf("Failed to resolve TCP address: %v", err)
+		}
+		httpListener, err := net.ListenTCP("tcp", httpAddr)
+		if err != nil {
+			logFatalf("Failed to start TCP listener: %v", err)
+		} else {
+			logInfof("Started HTTP listener on %s\n", httpAddr)
+		}
+		err = httpServer.Serve(httpListener)
+		if err != nil && err != http.ErrServerClosed {
+			logFatalf("Failed to start HTTP server: %v", err)
+		}
+	}()
+	// Setup signal capturing.
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	// Waiting for SIGINT (Ctrl+C)
+	select {
+	case <-stop:
+		fmt.Println("\nShutting down the server...")
+
+		// Create a deadline for the shutdown process.
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		// Start shutdown.
+		if err := httpServer.Shutdown(ctx); err != nil {
+			fmt.Println("Error during server shutdown:", err)
+		}
+
+		// Additional cleanup code here...
+
+		fmt.Println("Server gracefully stopped.")
+	}
+}
