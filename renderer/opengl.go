d77bf8d9e76324b3178b398c94be5740b6409186
Niels-Ole Lambertz <git@niels-ole.com>
2025-03-29 21:39:22 +0100

    INITIAL COMMIT after a long time of absence

--- renderer/opengl.go
+++ renderer/opengl.go
@@ -9,11 +9,86 @@ import (
 	"net/http"
 	"strconv"
 
+	"git.sr.ht/~sbinet/gg"
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/nielsole/go-gl-osm/utils"
 )
 
 func HandleRenderRequestOpenGL(w http.ResponseWriter, r *http.Request, data *Data, maxTreeDepth uint32, mmapData *[]byte) {
+	z, x, y, ext, err := utils.ParsePath(r.URL.Path)
+	if ext != "png" {
+		http.Error(w, "Only png is supported", http.StatusBadRequest)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tile := Tile{
+		X: x,
+		Y: y,
+		Z: z,
+	}
+	bbox := getBoundingBox(tile)
+	// fmt.Printf("Bounding box is from Lat %f, Lon %f to Lat %f, Lon %f\n", bbox.Min.Lat, bbox.Min.Lon, bbox.Max.Lat, bbox.Max.Lon)
+
+	const S = 256
+	dc := gg.NewContext(S, S)
+
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	parentTile := tile
+	for tempZ := z; tempZ > maxTreeDepth; tempZ-- {
+		parentTile = parentTile.getParent()
+	}
+	wayIndices, ok := data.Tiles[parentTile.index()]
+	if !ok {
+		// Return 404
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	way := MapObject{Points: make([]Point, 0, data.MaxPoints)}
+	for _, wayReference := range *wayIndices {
+		err := ReadMapObject(mmapData, int64(wayReference), &way)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// way := data.MapObjects[wayReference]
+		visible := false
+		if !bbox.overlaps(way.BoundingBox) {
+			continue
+		}
+		//tags := way.Tags.Map()
+		for i, point := range way.Points {
+			// Print Lat and Lng of node
+			//fmt.Printf("Lat: %f, Lon: %f\n", node.Lat, node.Lon)
+
+			if bbox.contains(point) {
+				visible = true
+			}
+
+			// We know that all previous points were outside of bounds, so we can skip them
+			if i != 0 && visible {
+				dc.SetRGB(0, 0, 0)
+				dc.SetLineWidth(1)
+				previousPoint := way.Points[i-1]
+				currentPixel := pointToPixels(point, bbox, S)
+				previousPixel := pointToPixels(previousPoint, bbox, S)
+				dc.DrawLine(previousPixel.X, previousPixel.Y, currentPixel.X, currentPixel.Y)
+				dc.Stroke()
+			}
+		}
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	// var buf bytes.Buffer
+	// png.Encode(&buf, img) // Encode the image to PNG
+	// if err := dc.EncodePNG(w); err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
+
 	dataBytes := drawOffscreen()
 	w.Header().Set("Content-Type", "image/png")
 	w.Header().Set("Content-Length", strconv.Itoa(len(dataBytes)))
